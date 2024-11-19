de54792343ccc6adb6d72f665c4580abd19a1078
foolguyisme <b11001020@ctbc.edu.tw>
2024-11-19 17:20:22 +0800

    Update 轉成.py

--- 轉成.py
+++ 轉成.py
@@ -1,15 +1,15 @@
 import opencc
 
-# 創建轉換器
+# 轉換
 converter = opencc.OpenCC('t2s')  # 繁體轉簡體
 
-# 讀取文件
+# 文件
 with open('2023.08.15 (1).srt', 'r', encoding='utf-8') as file:
     content = file.read()
 
-# 進行繁體到簡體的轉換
+# 進行繁體到簡體轉換
 simplified_content = converter.convert(content)
 
-# 將轉換後的內容寫回新文件
+# 重寫進去
 with open('your_file_simplified.srt', 'w', encoding='utf-8') as file:
     file.write(simplified_content)
